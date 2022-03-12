b81c75ed187af5cc98dd3b94b9c94c00c1060802
jojo59516 <ycbuaa2011@buaa.edu.cn>
2022-03-12 17:29:36 +0800

    add example

--- Assets/Lua/Main.lua
+++ Assets/Lua/Main.lua
@@ -1,6 +1,8 @@
+local UnityProfilerMarker = require("unity.profiling.ProfilerMarker")
+
 --主入口函数。从这里开始lua逻辑
-function Main()					
-	print("logic start")	 		
+function Main()
+	print("logic start")
 end
 
 --场景切换通知
@@ -8,6 +10,15 @@ function OnLevelWasLoaded(level)
 	collectgarbage("collect")
 	Time.timeSinceLevelLoad = 0
 end
+
+local update_marker = UnityProfilerMarker.Get("Update")
+function Update()
+    update_marker:Begin()
+    print(Time.frameCount)
+    update_marker:End()
+end
+
+UpdateBeat:AddListener(UpdateBeat:CreateListener(Update))
 
 function OnApplicationQuit()
 end
